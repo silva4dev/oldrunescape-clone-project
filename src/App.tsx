997a0d2b1d37acc1441a0c0fe6635e4158240b3c
silva4dev <web4dev.lucas@outlook.com>
2023-04-16 21:43:12 -0300

    inicial

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { makeStyles } from "@fluentui/react-components";
+import backgroundImage from "./assets/images/background.jpg";
+import logo from "./assets/images/logo.png";
+
+const useStyles = makeStyles({
+  wrapper: {
+    height: "100vh",
+    backgroundColor: "#000",
+    color: "#fff",
+  },
+  container: {
+    maxWidth: "900px",
+    paddingRight: "80px",
+    paddingLeft: "80px",
+    marginLeft: "auto",
+    marginRight: "auto",
+    paddingTop: "40px",
+    height: "100vh",
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundPositionX: "center",
+    backgroundPositionY: "top",
+  },
+  header: {
+    width: "max-content",
+  },
+});
+
+export function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.container}>
+        <div className={classes.header}>
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+import { App } from "./App";
+import { Helmet } from "react-helmet";
+import { FluentProvider, teamsDarkTheme } from "@fluentui/react-components";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <FluentProvider theme={teamsDarkTheme}>
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Helmet>
+      <App />
+    </FluentProvider>
+  </React.StrictMode>,
+);
