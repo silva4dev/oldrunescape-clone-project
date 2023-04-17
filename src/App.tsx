3ff5656fbf18c63035433dd3df58e236c96f62f3
silva4dev <web4dev.lucas@outlook.com>
2023-04-17 01:49:03 -0300

    Alterando o componente app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { makeStyles } from "@fluentui/react-components";
 import backgroundImage from "./assets/images/background.jpg";
 import brand from "./assets/images/brand.png";
+import { makeStyles, useId, Input, Label } from "@fluentui/react-components";
+import React from "react";
 
 const useStyles = makeStyles({
   wrapper: {
@@ -23,15 +24,46 @@ const useStyles = makeStyles({
   header: {
     width: "max-content",
   },
+  searchBar: {
+    "> label": {
+      display: "block",
+      color: "#fff",
+      marginBottom: "10px",
+    },
+  },
 });
 
 export function App() {
-  const classes = useStyles();
+  const usernameId = useId("username");
+  const styles = useStyles();
+  const [username, setUsername] = React.useState("");
+
+  function handleKeyUp(event: React.KeyboardEvent): void {
+    const input = event.target as HTMLInputElement;
+    if (input.value != "" && event.key === "Enter") {
+      setUsername(input.value);
+    }
+  }
+
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.container}>
-        <div className={classes.header}>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.header}>
           <img src={brand} alt="brand" />
+        </div>
+        {username}
+        <div className={styles.searchBar}>
+          <Label htmlFor={usernameId} size="large">
+            Search Username
+          </Label>
+          <Input
+            id={usernameId}
+            name="username"
+            size="large"
+            style={{ width: "100%" }}
+            autoComplete="off"
+            onKeyUp={handleKeyUp}
+          />
         </div>
       </div>
     </div>
